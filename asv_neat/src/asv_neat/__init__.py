fd34bf75eeef686f8d6f1e24d51b39df76098a5e
Seyi-Daniel <seyi4christforever@gmail.com>
2025-11-10 13:18:49 -0600

    Refactor NEAT crossing project structure

--- asv_neat/src/asv_neat/__init__.py
+++ asv_neat/src/asv_neat/__init__.py
@@ -1,14 +1,24 @@
-"""Public interface for the simplified crossing scenario package."""
+"""Convenience exports for the NEAT-controlled COLREGs crossing project."""
+
 from .boat import Boat
 from .config import BoatParams, EnvConfig, TurnSessionConfig
 from .env import CrossingScenarioEnv
+from .hyperparameters import HyperParameters, apply_cli_overrides
+from .neat_training import (
+    EpisodeMetrics,
+    TrainingResult,
+    build_scenarios,
+    episode_cost,
+    evaluate_population,
+    simulate_episode,
+    train_population,
+)
 from .scenario import (
     STAND_ON_BEARINGS_DEG,
     CrossingScenario,
     ScenarioRequest,
     compute_crossing_geometry,
     iter_scenarios,
-    raw_agent_inputs,
     scenario_states_for_env,
 )
 
@@ -18,11 +28,19 @@ __all__ = [
     "EnvConfig",
     "TurnSessionConfig",
     "CrossingScenarioEnv",
+    "HyperParameters",
+    "apply_cli_overrides",
+    "EpisodeMetrics",
+    "TrainingResult",
+    "build_scenarios",
+    "episode_cost",
+    "evaluate_population",
+    "simulate_episode",
+    "train_population",
     "STAND_ON_BEARINGS_DEG",
     "CrossingScenario",
     "ScenarioRequest",
     "compute_crossing_geometry",
     "iter_scenarios",
-    "raw_agent_inputs",
     "scenario_states_for_env",
 ]
